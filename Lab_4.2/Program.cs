d630e370ca7e00bc438bda8e6b93332a1f502096
Алексей Бочкарев <aleksey20024912@outlook.com>
2021-12-28 01:06:58 +0700

    menu fix

--- Lab_4.2/Program.cs
+++ Lab_4.2/Program.cs
@@ -11,6 +11,42 @@ namespace Lab_4._2
         private static double A, B;
         static void Main(string[] args)
         {
-        }
-    }
+			while (true)
+			{
+				PrintMenu();
+				DoAction(Console.ReadLine());
+			}
+		}
+
+		static void PrintMenu() =>
+			Console.WriteLine("1: Input A\n2: Input B\n3: +\n4: -\n5: *\n6: /");
+
+		static void DoAction(string input)
+		{
+			switch (input)
+			{
+				case "1":
+					InputA(Console.ReadLine());
+					break;
+				case "2":
+					InputB(Console.ReadLine());
+					break;
+				case "3":
+					Plus(A, B);
+					break;
+				case "4":
+					Minus(A, B);
+					break;
+				case "5":
+					Multiply(A, B);
+					break;
+				case "6":
+					Divide(A, B);
+					break;
+				default:
+					Console.WriteLine("Unrecognized command.");
+					break;
+			}
+		}
+	}
 }
